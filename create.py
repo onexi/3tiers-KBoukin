616fb77795e21c44362b8605e2574b70ec80aba8
KatyaB <kboukin@mit.edu>
2023-03-15 16:01:59 -0400

    finished create.py

--- create.py
+++ create.py
@@ -1,48 +1,47 @@
+#Create the server and route to receive data from the browser and send it to the database
+# imports
+from flask import Flask, render_template, request, redirect
 import sqlite3
 
-#connect to database education.db
-conn = sqlite3.connect('data/education.db')
+# connect to db and get cursor
+connection = sqlite3.connect("data/education.db", check_same_thread=False)
+cursor = connection.cursor()
 
-#create a cursor
-c = conn.cursor()
+# web application
+app = Flask(__name__)
 
-#create input for table students
-studentID = input("Enter student ID: ")
-collegeID = input("Enter college ID: ")
-firstName = input("Enter first name: ")
-lastName = input("Enter last name: ")
-birthDate = input("Enter birth date: ")
-email = input("Enter email: ")
-city = input("Enter city: ")
-region = input("Enter region: ")
-country = input("Enter country: ")
+@app.route('/createform')
+def add():
+        return render_template('create.html')
+#we want to be able to catch what the user inputs in the form through the submit button
+@app.route('/create', methods=['POST']) #we want to be able to catch what the user inputs in the form through the submit button
+def create():
+        # get data from form Colleges and Students
+        education = request.form
+        #for Colleges 
+        college_id = education['college_id'] 
+        name = education['name']
+        students = education['students']
+        city = education['city']
+        region = education['region']
+        country = education['country']
+        #for Students
+        student_id = education['student_id']
+        firstname = education['firstname']
+        lastname = education['lastname']
+        birth_date = education['birth_date']
+        email = education['email']
+        city_s = education['city_s']
+        region_s = education['region_s']
+        country_s = education['country_s']
+        college_id = education['college_id']
+        # insert data into table
+        cursor.execute("INSERT INTO colleges VALUES (?, ?, ?, ?, ?, ?)", (college_id, name, students, city, region, country))
+        cursor.execute("INSERT INTO students VALUES (?, ?, ?, ?, ?, ?, ?, ?)", (student_id, firstname, lastname, birth_date, email, city_s, region_s, country_s, college_id))
+        connection.commit()
+        return redirect('/read')
+#start server
+if __name__ == '__main__':
+        app.run(debug=True, port=5000)
 
-#create input for table colleges
-collegeID = input("Enter college ID: ")
-name = input("Enter college name: ")
-students = input("Enter number of students: ")
-city = input("Enter city: ")
-region = input("Enter region: ")
-country = input("Enter country: ")
-
-#writing to table students
-c.execute("INSERT INTO students VALUES (:studentID, :collegeID, :firstName, :lastName, :birthDate, :email, :city, :region, :country)",
-        { 'studentID': studentID, 'collegeID': collegeID, 'firstName': firstName, 'lastName': lastName, 'birthDate': birthDate, 'email': email, 'city': city, 'region': region, 'country': country})
-
-#writing to table colleges
-c.execute("INSERT INTO colleges VALUES (:collegeID, :name, :students, :city, :region, :country)",
-        { 'collegeID': collegeID, 'name': name, 'students': students, 'city': city, 'region': region, 'country': country})
-
-#commit changes
-conn.commit()
-
-#verify changes
-c.execute("SELECT * FROM students")
-print(c.fetchall())
-
-c.execute("SELECT * FROM colleges")
-print(c.fetchall())
-
-#close connection
-cursor.close()
-conn.close()
+# Path: read.py

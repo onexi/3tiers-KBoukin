808cc6d05c30b3dea1cd6f8f56dc23bdbf6a4cc5
KBoukin <46872098+KBoukin@users.noreply.github.com>
2023-03-17 15:50:44 +0000

    debugged create and read

--- create.py
+++ create.py
@@ -2,6 +2,7 @@
 # imports
 from flask import Flask, render_template, request, redirect
 import sqlite3
+from datetime import datetime
 
 # connect to db and get cursor
 connection = sqlite3.connect("data/education.db", check_same_thread=False)
@@ -19,25 +20,29 @@ def create():
         # get data from form Colleges and Students
         education = request.form 
         #for Colleges 
-        college_id = education['college_id'] 
+        college_id = int(education['college_id'])
         name = education['name']
-        students = education['students']
+        students = int(education['students'])
         city = education['city']
         region = education['region']
         country = education['country']
         #for Students
-        student_id = education['student_id']
+        student_id = int(education['student_id'])
+        print(student_id) #print to check
         firstname = education['firstname']
         lastname = education['lastname']
-        birth_date = education['birth_date']
+        #cast string to date
+        birth_date = datetime.strptime(education['birth_date'], '%Y-%m-%d')
+        print(birth_date) #print to check
         email = education['email']
         city_s = education['city_s']
         region_s = education['region_s']
         country_s = education['country_s']
-        college_id = education['college_id']
+        college_id = int(education['college_id'])
         # insert data into table
         cursor.execute(f"INSERT INTO colleges VALUES (?, ?, ?, ?, ?, ?)", (college_id, name, students, city, region, country))
-        cursor.execute(f"INSERT INTO students VALUES (?, ?, ?, ?, ?, ?, ?, ?)", (student_id, firstname, lastname, birth_date, email, city_s, region_s, country_s, college_id))
+        cursor.execute(f"INSERT INTO students VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", (student_id, firstname, lastname, birth_date, email, city_s, region_s, country_s, college_id))
+        print(cursor.execute(f"INSERT INTO students VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", (student_id, firstname, lastname, birth_date, email, city_s, region_s, country_s, college_id))) #print to check
         connection.commit() #save changes
         return redirect('/read') #redirect to the read page
 #start server
